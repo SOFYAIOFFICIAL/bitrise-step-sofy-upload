dcc699d461069437b02ecb1418227145418e571c
Steven Elliott <stevenelliott.developer@gmail.com>
2023-10-27 09:18:54 -0500

    Adding Application Guid parameter to bitrise step

--- step.sh
+++ step.sh
@@ -5,7 +5,8 @@ set -ex
 echo "build path "${build_path}""
 curl --location --request POST 'https://api.sofy.ai/api/AppTests/buildUpload' \
 --header "SubscriptionKey: ${subscription_key}" \
---form "applicationFile=@"${build_path}""
+--form "applicationFile=@"${build_path}"" \
+--form "ApplicationGuid=="${application_guid}""
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
