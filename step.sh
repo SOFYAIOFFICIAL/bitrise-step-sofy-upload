8793eca8885bf24091c39118a677047c84e0a167
Steven Elliott <stevenelliott.developer@gmail.com>
2024-02-05 14:57:14 -0600

    Fixed syntax issue

--- step.sh
+++ step.sh
@@ -6,7 +6,7 @@ echo "build path "${build_path}""
 curl --location --request POST 'https://public.sofy.ai/parser-microservice/build-upload' \
 --header "x-sofy-auth-key: ${subscription_key}" \
 --form "applicationFile=@"${build_path}"" \
---form 'ApplicationGUID="${application_guid}""
+--form "ApplicationGUID="${application_guid}""
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
